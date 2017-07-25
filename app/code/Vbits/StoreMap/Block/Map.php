a3d9d9c175e364fdaaffa283c15d04915a289759
NhanNguyen <nhan.tobe.95@gmail.com>
2017-07-25 03:42:49 -0700

    module map store

---
+++ app/code/Vbits/StoreMap/Block/Map.php
@@ -0,0 +1,9 @@
+<?php
+namespace Vbits\StoreMap\Block;
+ 
+class Map extends \Magento\Framework\View\Element\Template
+{
+    public function getHelloWorldTxt()
+    {
+        return 'Hello world!';
+    }
